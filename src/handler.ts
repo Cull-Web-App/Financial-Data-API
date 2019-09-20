13c1761e7e443f5a0d38635367e838a90c445eec
haines10 <mleehaines@gmail.com>
2019-09-20 15:29:17 -0500

    Single Ticker Time-Interval based API is working

--- src/handler.ts
+++ src/handler.ts
@@ -1,11 +1,38 @@
 import { Handler, APIGatewayProxyEvent, Context, APIGatewayProxyResult } from 'aws-lambda';
+import * as req from 'request-promise';
+import { reject } from 'bluebird';
+
+let apiKey = process.env.API_KEY;
 
 export const getTickers: Handler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
-    console.log('The current ticker is: '+ event.queryStringParameters);
+    if (event.queryStringParameters) {        
+        const ticker: string = event.queryStringParameters.ticker;
+        var interval: string;
+        if (event.queryStringParameters.interval) {
+            interval = event.queryStringParameters.interval;
+        }
+        else{
+            interval = "1m";
+        }
+        let options = {
+            uri: 'https://cloud.iexapis.com/v1/stock/' + ticker + '/chart/' + interval,
+            qs: {
+                token: apiKey
+            },
+            json: true
+        };
+
+        let resp = await req.get(options);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(resp)
+        };
+    }
     return {
-        statusCode: 200,
+        statusCode: 400,
         body: JSON.stringify({
-            message: 'Worked'
+            message: 'Ticker was not provided or was invalid'
         })
-    };
+    };    
+    
 }
