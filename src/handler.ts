0818fa66efaf5b8fed8dcbdd0a3cf616409ea3d3
haines10 <mleehaines@gmail.com>
2019-10-02 19:57:47 -0500

    Removed request-promise & bluebird

--- src/handler.ts
+++ src/handler.ts
@@ -1,50 +1,38 @@
 import { Handler, APIGatewayProxyEvent, Context, APIGatewayProxyResult } from 'aws-lambda';
-import * as req from 'request-promise';
-import { reject } from 'bluebird';
+import axios, {AxiosResponse, AxiosRequestConfig, AxiosPromise} from 'axios';
 import { candleStickData } from './models';
 
-let apiKey = process.env.API_KEY;
+const apiKey = process.env.API_KEY;
 
-export const candlestickData: Handler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
-    if (event.queryStringParameters) {        
-        const ticker: string = event.queryStringParameters.ticker;
-        var interval: string;
-
-        if (event.queryStringParameters.interval) {
-            interval = event.queryStringParameters.interval;
-        }
-        else{
-            interval = "1m";
-        }
-        let options = {
-            uri: 'https://cloud.iexapis.com/v1/stock/' + ticker + '/chart/' + interval,
-            qs: {
+export const candlestickData: Handler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => 
+{
+    if (event.queryStringParameters) 
+    {        
+        const {ticker, interval} = event.queryStringParameters;
+        const config: AxiosRequestConfig = {
+            baseURL: 'https://cloud.iexapis.com/v1/stock/',
+            url: ticker + '/chart/' + (interval || '1m'),
+            method: 'get',
+            params: {
                 token: apiKey
             },
-            json: true
+            responseType: 'json'
         };
 
-        try{
-            let resp = await req.get(options);
-            var returnData;
-            console.log(interval);
-            if (interval == "1mm" || interval == "5dm"){
-                returnData = resp.map(data => <candleStickData>({date: data.date,time:data.minute, openPrice: data.open, closePrice: data.close}));
-            }
-            else{
-                returnData = resp.map(data => <candleStickData>({date: data.date, openPrice: data.open, closePrice: data.close}));
-            }
-                    
-
+        try
+        {
+            const resp = await axios(config);
+            const returnData = resp.data.map(data => <candleStickData>({date: data.date,time:data.minute, openPrice: data.open, closePrice: data.close}));
             return {
                 statusCode: 200,
                 body: JSON.stringify(returnData)
             };
         }
-        catch(e){
+        catch(e)
+        {
             return {
                 statusCode: 400,
-                body:e
+                body: e
             };
         }
         
