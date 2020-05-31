cbd05880d85bdddd088c190f86eba8d3de647d16
cbhubbard <hubbard8@gmail.com>
2020-05-31 11:44:17 -0500

    Fix spelling

--- src/config/inversify.config.ts
+++ src/config/inversify.config.ts
@@ -20,7 +20,7 @@ import {
 import { SERVICE_IDENTIFIERS } from '../constants';
 
 // Bind the data to the interfaces for injection -- use singletons for the services since they don't need multiple instances
-const asynContainerModule: AsyncContainerModule = new AsyncContainerModule(async (bind: interfaces.Bind) => {
+const asyncContainerModule: AsyncContainerModule = new AsyncContainerModule(async (bind: interfaces.Bind) => {
     bind<IAppConfigurationService>(SERVICE_IDENTIFIERS.IAPP_CONFIGURATION_SERVICE).to(AppConfigurationService).inSingletonScope();
     bind<IAWSConfigurationService>(SERVICE_IDENTIFIERS.IAWS_CONFIGURATION_SERVICE).to(AWSConfigurationService).inSingletonScope();
     bind<IQuoteService>(SERVICE_IDENTIFIERS.IQUOTE_SERVICE).to(QuoteService).inSingletonScope();
@@ -32,5 +32,5 @@ const asynContainerModule: AsyncContainerModule = new AsyncContainerModule(async
 // Create the DI Container and export -- runs async for perf and possible async injection purposes
 export const container: Container = new Container();
 (async () => {
-    await container.loadAsync(asynContainerModule);
+    await container.loadAsync(asyncContainerModule);
 })();
